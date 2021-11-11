9e78f231bd8b2cda1ff8943e40c557a80a5a605a
Jyue <s310191@hotmail.com>
2021-11-11 23:38:59 +0800

    [AppCheck]add AppCheck

--- app/src/main/java/com/jay/demo/MainActivity.kt
+++ app/src/main/java/com/jay/demo/MainActivity.kt
@@ -1,22 +1,65 @@
 package com.jay.demo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory
+import com.google.firebase.database.*
+import com.jay.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        insertMessageToFireBaseDatabase()
+        FirebaseApp.initializeApp(/*context=*/this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            SafetyNetAppCheckProviderFactory.getInstance()
+        )
+
+        initView()
     }
 
-    private fun insertMessageToFireBaseDatabase() {
-        val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-        val ref: DatabaseReference = database.getReference("message")
+    private fun initView() {
+        binding.btnInsertToFirebase.setOnClickListener {
+            insertMessageToFireBaseDatabase(
+                binding.edInsertKey.text.toString(),
+                binding.edInsertValue.text.toString()
+            )
+        }
 
-        ref.setValue("AppCheck Demo")
+        binding.btnReadFromFirebase.setOnClickListener {
+            readMessageFromFireBaseDatabase(binding.edReadKey.text.toString())
+        }
+    }
+
+    private fun insertMessageToFireBaseDatabase(key: String, value: String) {
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+        val ref: DatabaseReference = database.getReference(key)
+
+        ref.setValue(value)
+    }
+
+    private fun readMessageFromFireBaseDatabase(key: String) {
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+        val ref: DatabaseReference = database.getReference(key)
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.value
+                binding.tvMessage.text = value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(MainActivity::class.java.simpleName, error.message)
+            }
+
+        })
     }
 }
